2de6c89e41e7713973ec6016dd227582df1de4e5
Patrick Dowling <patrick_dowling@pm.me>
2021-01-25 13:09:15 -0500

    Rearranged project structure, hopefully didn't break anything.  Added a few new files.

---
+++ Godot.Signals/CanvasItem/Node2D/AnimatedSprite.cs
@@ -0,0 +1,14 @@
+﻿namespace Godot {
+    public static partial class Signals {
+        public class AnimatedSprite : Node2D {
+            /// <summary>
+            /// Emitted when the animation is finished (when it plays the last frame).
+            /// If the animation is looping, this signal is emitted every time the last frame is drawn.
+            /// </summary>
+            public const string AnimationFinished = "animation_finished";
+
+            /// <summary> Emitted when <see cref="Godot.AnimatedSprite.Frame"/> changed. </summary>
+            public const string FrameChanged = "frame_changed";
+        }
+    }
+}
